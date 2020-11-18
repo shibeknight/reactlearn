de858d4212bfdf9b2e2161c15dd58b92991b1ce7
Daniel Antonio Ceballos Flores <danielc@linqstream.com>
2020-11-18 14:26:31 -0500

    added useEffect example with api call

--- src/views/Portfolio.js
+++ src/views/Portfolio.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
-function Portfolio(props) {
+function Portfolio() {
+  const [userlist, setuserlist] = useState([])
+
+  useEffect(() => {
+    console.log('use Effect!')
+    findusers()
+  },[]);
+
+  async function findusers (){
+    const users = await axios.get('https://randomuser.me/api/?results=100');
+    console.log(users.data.results);
+    setuserlist(users.data.results);
+  }
+
   return (
     <div>
       <h1>We are in the portfolio component</h1>
+      <p>This is where the random users will be:</p>
+      {/* {JSON.stringify(userlist)} */}
+      {userlist.map(user => 
+        <li>{user.name.first}</li>
+      )}
     </div>
   );
 }
